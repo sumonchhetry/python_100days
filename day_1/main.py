36d682ffd499cfa81b70bb779c1de29c10bc6ec3
Sumon Chhetry <sumonchhetry1@gmail.com>
2022-05-07 00:21:25 +0530

    day 1

---
+++ day_1/main.py
@@ -0,0 +1,8 @@
+# Band name generator
+city = input("What\'s the name of the city you grew up in ? \n")
+
+pet = input("What\'s the name of your pet ? \n")
+
+band_name = pet + "\'s " + city
+
+print("Your band name could be \n" + band_name)
